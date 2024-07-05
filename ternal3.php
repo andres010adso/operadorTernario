255a2e52cf4787c5d1d1c3fa590689c7d01d7cd6
andres010adso <andresorlandoguzman@gmail.com>
2024-07-05 16:52:04 -0500

    subiendo operacion 3 acabada

--- ternal3.php
+++ ternal3.php
@@ -1,3 +1,8 @@
 <?php
 $largo = readline("Ingrese el largo del terreno en metros: ");
 $ancho = readline("Ingrese el ancho del terreno en metros: ");
+$areaTotal = $largo * $ancho;
+$mensaje = ($areaTotal >= 1000) ? "El terreno es adecuado para cultivos extensivos." : 
+"El terreno es más adecuado para cultivos de jardín o huerto.";
+echo "\n"
+echo $mensaje
